3061d0b468e0cee3ad0446d167b28bcd351f2219
Josh Comeau <joshwcomeau@gmail.com>
2014-04-17 15:08:24 -0400

    Added some data to database. Modelsimplemented with associations.

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140417181221) do
+ActiveRecord::Schema.define(version: 20140417184443) do
+
+  create_table "reservations", force: true do |t|
+    t.integer  "restaurant_id"
+    t.integer  "user_id"
+    t.datetime "start_time"
+    t.integer  "seats"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "restaurants", force: true do |t|
     t.string   "name"
     t.integer  "capacity"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
