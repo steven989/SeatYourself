04ea79936dc4e0bb0770990b60c64c26fd16db66
Josh Comeau <joshwcomeau@gmail.com>
2014-04-21 13:46:50 -0400

    Validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,10 @@
 class Review < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :restaurant
+
 	validates :title, presence: true
 	validates :content, presence: true
 	validates :rating, presence:true
 
-    belongs_to :user
-    belongs_to :restaurant
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ActiveRecord::Base
   has_many :restaurants, through: :reservations
   has_many :reviews
 
+  validates :email, :password, :first_name, :last_name, presence: true
+  validates :email, format: { with: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,12}/, message: "must be a valid email address."}
+
   has_secure_password
 
 end
