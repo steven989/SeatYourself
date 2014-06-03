f96d819ad89b21198aeb178d9f728b3a91cf0c89
Josh Comeau <joshwcomeau@gmail.com>
2014-06-03 10:54:46 -0400

    Fixed a bug where email addresses needed to be ALL CAPS.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   has_many :reviews
 
   validates :email, :password, :first_name, :last_name, presence: true
-  validates :email, format: { with: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,12}/, message: "must be a valid email address."}
+  validates :email, format: { with: /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,12}/i, message: "must be a valid email address."}
 
   has_secure_password
 
